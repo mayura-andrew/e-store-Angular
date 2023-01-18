6f47a6e66783f4588f9450060d16bf9e404832a8
mayura-alahakoon <mayuraalahakoon@gmail.com>
2023-01-18 13:00:49 +0530

    initial commit

---
+++ src/app/pages/home/components/filters/filters.component.ts
@@ -0,0 +1,38 @@
+import { Component, EventEmitter, OnDestroy, OnInit, Output } from '@angular/core';
+import { StoreService } from 'src/app/services/store.service';
+import { Subscription } from 'rxjs';
+@Component({
+  selector: 'app-filters',
+  templateUrl: './filters.component.html',
+ 
+})
+export class FiltersComponent implements OnInit, OnDestroy {
+
+  @Output() showCategory = new EventEmitter<string>();
+  
+  categoriesSubscription: Subscription | undefined;
+  categories : Array<string> | undefined;
+
+  constructor(private storeService: StoreService) {
+
+  }
+
+  ngOnInit(): void {
+   this.categoriesSubscription =  this.storeService.getAllCategories() 
+    .subscribe((response) => {
+      this.categories = (response);
+    });
+  }
+
+  onShowCategory(category: string): void {
+    this.showCategory.emit(category);
+    
+  }
+
+  ngOnDestroy(): void {
+    if (this.categoriesSubscription) {
+      this.categoriesSubscription.unsubscribe();
+    }
+  }
+}
+
